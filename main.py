c76f79815017b4bf1ac2347ebfafb04c4315903b
TrevStoke <trevor@trevweb.me.uk>
2014-06-22 03:04:45 +0100

    Added a basic Login/Logout.

--- main.py
+++ main.py
@@ -14,11 +14,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+from google.appengine.api import users
 import webapp2
+import jinja2
+import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        user = users.get_current_user()
+        if user:
+            login_url = users.create_logout_url(self.request.uri)
+            login_text = 'Logout'
+        else:
+            login_url = users.create_login_url(self.request.uri)
+            login_text = 'Login'
+
+        template_values = {
+            'login_url': login_url,
+            'login_text': login_text,
+            'user': user,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+        self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
